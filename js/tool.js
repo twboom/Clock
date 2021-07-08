94a225ca20d3335a765fe72ea5f1ab640841be94
Thijs Boom <twboom@hotmail.nl>
2021-07-08 21:48:07 +0200

    Got clock working

--- js/tool.js
+++ js/tool.js
@@ -7,9 +7,8 @@ class Clock {
         this.config = config
         this.display = config.display;
 
-        this.time = new Date();
         this.clock = document.createElement('p');
-        this.elements = [];
+        this.elements = {};
 
 
         // Generating the clock
@@ -17,38 +16,60 @@ class Clock {
         const elems = this.elements;
 
         const cont = this.clock;
+        cont.classList = 'clock container'
 
         // Generating the required elements
         if (disp.hours) {
             const hours = document.createElement('span');
             hours.classList = 'clock hours';
-            elems.push(hours);
+            this.elements.hours = hours;
         };
 
         if (disp.minutes) {
             const minutes = document.createElement('span');
             minutes.classList = 'clock minutes';
-            elems.push(minutes);
+            this.elements.minutes = minutes;
         };
 
         if (disp.seconds) {
             const seconds = document.createElement('span');
             seconds.classList = 'clock seconds';
-            elems.push(seconds);
+            this.elements.seconds = seconds;
         };
 
         // Adding the elements
-        for (let i = 0; i < elems.length; i++) {
+        for (let i = 0; i < Object.values(elems).length; i++) {
 
             if (i !== 0) {
                 const seperator = document.createElement('span');
                 seperator.classList = 'clock seperator';
+                seperator.innerText = ':'
                 cont.appendChild(seperator);
             };
 
-            cont.appendChild(elems[i]);
+            cont.appendChild(Object.values(this.elements)[i]);
 
         };
+
+    };
+
+    // Append clock to the page
+    create() {
+        this.container.appendChild(this.clock);
+    };
+
+
+    // Controls
+    start() {
+        this.tick()
+        this.interval = setInterval(_ => {
+            this.tick()
+        }, this.config.precision)
+    };
+
+    pause() {
+
+        clearInterval(this.interval)
 
     };
 
@@ -56,27 +77,43 @@ class Clock {
     // Tick
     tick() {
 
+        const now = new Date();
 
+        for (const [key, value] of Object.entries(this.elements)) {
 
-    }
+            let txt = '';
 
+            // Set string to the correct contents
+            switch (key) {
 
-    // Append clock to the page
-    create() {
-        this.container.appendChild(cont);
-    };
+                case 'hours':
+                    txt = now.getHours() 
+                    break;
 
+                case 'minutes':
+                    txt = now.getMinutes() 
+                    break;
 
-    // Controls
-    start() {
+                case 'seconds':
+                    txt = now.getSeconds() 
+                    break;
 
-        this.tick = setInterval(tick, this.config.precision)
+            };
 
-    };
+            // Transform string to make it better
 
-    pause() {
+            txt = txt.toString()
 
-        clearInterval(this.tick)
+            if (txt.length < 2) {
+                txt = '0' + txt;
+            }
+
+            // Set element content to new text
+
+            if (value.innerText === txt) { continue }
+            else { value.innerText = txt };            
+
+        };
 
     };
 
@@ -84,6 +121,6 @@ class Clock {
 
 
 /* 
-    Below are utility functions used by Perip itself
+    Below are utility functions used by the tool itself
 */
 const utility = [];
