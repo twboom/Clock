eb4503f6d1d4e216789776efb60da15ea5523c3c
Thijs Boom <twboom@hotmail.nl>
2021-07-13 21:26:46 +0200

    Started work on logic

--- js/countdown.js
+++ js/countdown.js
@@ -1,6 +1,32 @@
 // Countdown clock
 class Countdown {
 
-    constructor() {}
+    constructor(target) {
 
-}
+        // Set the date to 'now' if it is left empty
+        // Just to prevent errors
+        // BONUS: It makes a timer when no time is specified
+        if (target === undefined) { target = Date.now() };
+
+        this.date = new Date(target); // The targeted time to countdown to
+        this.target = this.date.getTime(); // The target time in ms
+        
+        this.left = this.target - Date.now();
+
+        this.elements = {}; // Holder for the DOM objects
+
+        // Generating the DOM objects
+
+        
+
+    };
+
+    tick() {
+
+        this.left = this.target - Date.now();
+
+
+
+    }
+
+};
